1b0c39036dab0cbd404885b44970cdb3bd131c83
vgubarkov <xonixx@gmail.com>
2017-09-03 19:24:35 +0300

    readme

--- cibuild.sh
+++ cibuild.sh
@@ -4,4 +4,5 @@ set -e # halt script on error
 
 bundle exec jekyll build
 mv _site site # looks like deploy phase doesn't like underscores
+rm site/*.sh
 echo www.icosurance.com > site/CNAME # custom domain
